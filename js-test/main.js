009c7186bed30cdc9bf5e817e7387d1127b43470
chajaesik01 <ckwotlr@dongguk.edu>
2024-10-14 17:10:09 +0900

    reupload

---
+++ js-test/main.js
@@ -0,0 +1,43 @@
+//산술 연산자
+
+console.log(1+2);
+console.log(5-7);
+console.log(3 * 4);
+console.log(5/2);
+// % 기호를 사용하여 나머지 값 획득
+console.log(5%2);
+
+//할당 연산자
+
+let a = 2;
+//a = a + 1;
+a +=1
+console.log(a)
+
+//비교 연산자
+const a2 = 1;
+const b2 = 1;
+
+console.log(a2===b2);
+
+function isEqual(x, y){
+    return x === y;
+}
+
+console.log(isEqual(1, 1));
+console.log(isEqual(2,'2'));
+
+const a3 = 1
+const b3 = 3;
+
+console.log(a3 !== b3);
+console.log(a < b);
+
+//삼항 연산자
+
+const b = 1 <2
+
+console.log(b ? '참' : '거짓');
+
+
+
