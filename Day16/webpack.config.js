350e142845e7025cc399554980e67f2096826053
chajaesik01 <ckwotlr@dongguk.edu>
2024-11-05 11:31:28 +0900

    Day17 file upload

--- Day16/webpack.config.js
+++ Day16/webpack.config.js
@@ -45,3 +45,20 @@ module.exports = {
         host: 'localhost'
     }
 }
+
+
+// 번들러 : tes, es6,es8 등..을 웹에서 동작하는 hmtl.css,.js로 변환하는 작업을 해주는 것
+
+
+// 1. pacel = 구성없는 단순한 자동 번들링, 소/중형 프로젝트에 적합
+// 2. weppack = 매우 꼼꼼한 구성 중/대형 프로젝틍 적합
+
+
+//   "browserslist":[
+//     "> 1%",
+//     "last 2 versions"
+//   ]
+
+// 전세계 1%이상 사용하는 브라우저에 마지막 업데이트2버젼까지 지원하겠다.
+
+// 위 사항을 명시하여 autoprefixer(공급사항 접두사) 설정
