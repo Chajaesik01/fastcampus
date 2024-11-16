87003a98cc539bc16432f2f07d251d8cfc662045
chajaesik01 <ckwotlr@dongguk.edu>
2024-11-16 14:46:22 +0900

    snapshot으로서의 state

---
+++ React공식문서따라하기/상호작용성 더하기/snapshot으로서의 state/quiz.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+export default function TrafficLight() {
+  const [walk, setWalk] = useState(true);
+
+  function handleClick() {
+    setWalk(!walk);
+    walk === true ? alert('다음은 정지입니다') : alert('다음은 걷기입니다')
+    
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        Change to {walk ? 'Stop' : 'Walk'}
+      </button>
+      <h1 style={{
+        color: walk ? 'darkgreen' : 'darkred'
+      }}>
+        {walk ? 'Walk' : 'Stop'}
+      </h1>
+    </>
+  );
+}
