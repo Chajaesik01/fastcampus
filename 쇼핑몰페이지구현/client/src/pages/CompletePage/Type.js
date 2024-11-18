263374f5dcfb85268d7d41be3beae29b5c0f7c52
chajaesik01 <ckwotlr@dongguk.edu>
2024-11-18 20:12:59 +0900

    09

--- 쇼핑몰페이지구현/client/src/pages/CompletePage/Type.js
+++ 쇼핑몰페이지구현/client/src/pages/CompletePage/Type.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './Products';
 import Options from './Options';
+import ErrorBanner from './ErrorBanner';
 const Type = ({ orderType }) => {
     const [items, setItems] = useState([]);
 
+    const[error, setError] = useState(false);
     useEffect(() => {
         loadItems(orderType);
     }, [orderType]);
@@ -14,6 +16,7 @@ const Type = ({ orderType }) => {
             const response = await axios.get(`http://localhost:4000/${type}`);
             setItems(response.data);
         } catch (error) {
+            setError(true);
             console.log(error);
         }    
     };
@@ -27,6 +30,12 @@ const Type = ({ orderType }) => {
             imagePath={item.imagePath}
         />
     ));
+
+    if(error){
+        return (
+           <ErrorBanner message ="에러가 발생했습니다"/>
+        )
+    }
 
     return (
         <div>
