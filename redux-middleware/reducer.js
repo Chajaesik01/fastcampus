51e1e8cb9ec18a499735a1a25adb4145991a7b80
chajaesik01 <ckwotlr@dongguk.edu>
2025-01-29 16:24:16 +0900

    middleware 만들기

---
+++ redux-middleware/reducer.js
@@ -0,0 +1,22 @@
+import * as Actions from './action-type.js';
+
+const InitializeState = {
+  message: 'app store',
+};
+
+export default function reducer(state = InitializeState, action) {
+  switch(action.type) {
+    case Actions.INCREASE_COUNTER:
+      if (action.payload){
+        return { ...state, counter: state.counter === undefined ? 1 : state.counter + action.payload };
+      }else{
+        return { ...state, counter: state.counter === undefined ? 1 : state.counter + 1 };
+      }
+    case Actions.DECREASE_COUNTER:
+      return { ...state, counter: state.counter === undefined ? 0 : state.counter - 1 };
+    case Actions.SET_COUNTER:
+      return { ...state, counter: action.payload };
+    default:
+       return { ...state };
+  }
+}
